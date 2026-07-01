0cc5967d1e33b8e2fc7b5c0567cea0c8ca82777f
SheffeyG <57262511+SheffeyG@users.noreply.github.com>
2026-07-01 18:06:30 +0800

    fix(build): embed app icon resource

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    embed_resource::compile("assets/app.rc", embed_resource::NONE);
+    println!("cargo:rerun-if-changed=assets/app.rc");
+    println!("cargo:rerun-if-changed=assets/app.ico");
+
+    embed_resource::compile_for("assets/app.rc", ["scremind"], embed_resource::NONE);
 }
